2656319729628b61dc5d486d77cebe5e15100bcf
Doug Melton <doug@mobiata.com>
2016-03-22 09:15:59 -0700

    Fixed code style to pass checkstyle build
    To make style failures not fatal, add this to the android block of build.gradle:
    
    lintOptions {
        abortOnError false
    }

--- trianglify/src/main/java/com/manolovn/trianglify/TrianglifyDrawable.java
+++ trianglify/src/main/java/com/manolovn/trianglify/TrianglifyDrawable.java
@@ -23,97 +23,97 @@ import com.manolovn.trianglify.triangulator.Triangulator;
  */
 public class TrianglifyDrawable extends Drawable {
 
-	private RegularPointGenerator pointGenerator;
-	private Triangulator triangulator;
-	private TriangleRenderer triangleRenderer;
+    private RegularPointGenerator pointGenerator;
+    private Triangulator triangulator;
+    private TriangleRenderer triangleRenderer;
 
-	private int width;
-	private int height;
+    private int width;
+    private int height;
 
-	private Vector<Point> points;
-	private Vector<Triangle> triangles;
+    private Vector<Point> points;
+    private Vector<Triangle> triangles;
 
-	// Used with triangulateInBackground
-	Boolean ready = false;
+    // Used with triangulateInBackground
+    Boolean ready = false;
 
-	public TrianglifyDrawable(int cellSize, int variance, int bleedX, int bleedY) {
-		super();
+    public TrianglifyDrawable(int cellSize, int variance, int bleedX, int bleedY) {
+        super();
 
-		pointGenerator = new RegularPointGenerator(cellSize, variance);
-		pointGenerator.setBleedX(bleedX);
-		pointGenerator.setBleedY(bleedY);
-		triangulator = new DelaunayTriangulator();
-		triangleRenderer = new TriangleRenderer();
-	}
+        pointGenerator = new RegularPointGenerator(cellSize, variance);
+        pointGenerator.setBleedX(bleedX);
+        pointGenerator.setBleedY(bleedY);
+        triangulator = new DelaunayTriangulator();
+        triangleRenderer = new TriangleRenderer();
+    }
 
-	@Override
-	protected void onBoundsChange(Rect bounds) {
-		this.width = bounds.width();
-		this.height = bounds.height();
-		triangulateInBackground();
-	}
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        this.width = bounds.width();
+        this.height = bounds.height();
+        triangulateInBackground();
+    }
 
-	@Override
-	public void draw(Canvas canvas) {
-		if (ready) {
-			triangleRenderer.render(triangles, canvas);
-		}
-	}
+    @Override
+    public void draw(Canvas canvas) {
+        if (ready) {
+            triangleRenderer.render(triangles, canvas);
+        }
+    }
 
-	@Override
-	public void setAlpha(int alpha) {
+    @Override
+    public void setAlpha(int alpha) {
 
-	}
+    }
 
-	@Override
-	public void setColorFilter(ColorFilter colorFilter) {
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
 
-	}
+    }
 
-	@Override
-	public int getOpacity() {
-		return PixelFormat.OPAQUE;
-	}
+    @Override
+    public int getOpacity() {
+        return PixelFormat.OPAQUE;
+    }
 
-	public void setVariance(int variance) {
-		pointGenerator.setVariance(variance);
-		triangulateInBackground();
-	}
+    public void setVariance(int variance) {
+        pointGenerator.setVariance(variance);
+        triangulateInBackground();
+    }
 
-	public void setCellSize(int cellSize) {
-		pointGenerator.setCellSize(cellSize);
-		triangulateInBackground();
-	}
+    public void setCellSize(int cellSize) {
+        pointGenerator.setCellSize(cellSize);
+        triangulateInBackground();
+    }
 
-	public void setColor(ColorBrewer color) {
-		triangleRenderer = new TriangleRenderer(new BrewerColorGenerator(color));
-		triangulateInBackground();
-	}
+    public void setColor(ColorBrewer color) {
+        triangleRenderer = new TriangleRenderer(new BrewerColorGenerator(color));
+        triangulateInBackground();
+    }
 
-	private void triangulateInBackground() {
-		TriangulateAsyncTask task = new TriangulateAsyncTask();
-		task.execute();
-	}
+    private void triangulateInBackground() {
+        TriangulateAsyncTask task = new TriangulateAsyncTask();
+        task.execute();
+    }
 
-	private Object lock = new Object();
+    private Object lock = new Object();
 
-	private class TriangulateAsyncTask extends AsyncTask<Void, Void, Void> {
+    private class TriangulateAsyncTask extends AsyncTask<Void, Void, Void> {
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			synchronized (lock) {
-				ready = false;
-				points = pointGenerator.generatePoints(width, height);
-				triangles = triangulator.triangulate(points);
-				ready = true;
-			}
-			return null;
-		}
+        @Override
+        protected Void doInBackground(Void... params) {
+            synchronized (lock) {
+                ready = false;
+                points = pointGenerator.generatePoints(width, height);
+                triangles = triangulator.triangulate(points);
+                ready = true;
+            }
+            return null;
+        }
 
-		@Override
-		protected void onPostExecute(Void aVoid) {
-			invalidateSelf();
-		}
-	}
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            invalidateSelf();
+        }
+    }
 
 }
